79219587eb91de5a16cb28a79ebc005942f2e978
adil-sultanov <sultanov.adil05@gmail.com>
2025-11-17 20:01:42 +0500

    init commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+async def root():
+    return 'hello world'
+
+@app.get('/aida')
+async def love():
+    return {'message': 'I LOVE YOU'}
+
+@app.get('/calc/{x}/{y}')
+def calc(x: int, y: int)->int:
+    return x + y
