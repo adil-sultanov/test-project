39fc604afb68ad180244a761cc2a42c82327f70a
adil-sultanov <sultanov.adil05@gmail.com>
2025-11-17 21:00:04 +0500

    more test stuff

--- main.py
+++ main.py
@@ -13,3 +13,14 @@ async def love():
 @app.get('/calc/{x}/{y}')
 def calc(x: int, y: int)->int:
     return x + y
+
+@app.get('/files/{file_path:path}')
+def get_path(file_path: str):
+    return file_path
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    print(skip, limit)
+    return fake_items_db[skip : skip + limit]
